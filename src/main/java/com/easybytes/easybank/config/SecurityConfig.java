26b4debb6aa3283f2a23d3bc85ada04bebb7f8d3
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-11-18 15:27:38 +0530

    EazyBank starter Project

---
+++ src/main/java/com/easybytes/easybank/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.easybytes.easybank.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myBalance","/myAccount","/myCards","/myLoans").authenticated()
+                .requestMatchers("/notices","/contact").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
