b773a744d15de67382eaf3dc7e08074b56906d5a
ch-sai-sumanth <saisumanth0531@gmail.com>
2024-12-17 11:23:11 +0530

    Added CORS Configuration for Client in filter chain

--- src/main/java/com/easybytes/easybank/config/ProdSecurityConfig.java
+++ src/main/java/com/easybytes/easybank/config/ProdSecurityConfig.java
@@ -2,6 +2,7 @@ package com.easybytes.easybank.config;
 
 import com.easybytes.easybank.ExceptionHandling.CustomAccessDeniedHandler;
 import com.easybytes.easybank.ExceptionHandling.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,11 +25,24 @@ public class ProdSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http    .sessionManagement(smc->smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
+        http     .cors(corsConfig-> corsConfig.configurationSource(new CorsConfigurationSource(){
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config =new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowCredentials(true);
+                        config.addAllowedHeader("*");
+                        config.addAllowedMethod("*");
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .sessionManagement(smc->smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
                 .requiresChannel(rcc->rcc.anyRequest().requiresSecure()) //allows only HTTPS requests
                 .csrf(csrfConfig->csrfConfig.disable())
                 .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myBalance","/myAccount","/myCards","/myLoans").authenticated()
+                .requestMatchers("/myBalance","/myAccount","/myCards","/myLoans","/user").authenticated()
                 .requestMatchers("/notices","/contact","/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(hbc->hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
